90c4045e071c10ef10dd6bc7268d6f297b7518aa
Vicente Téllez <vicente.tellez2401@alumnos.ubiobio.cl>
2026-09-17 00:59:00 -0300

    feat(auth): agrega middleware que autoriza un usuario, viendo su rol

---
+++ backend/src/middlewares/authorization.middleware.js
@@ -0,0 +1,22 @@
+import { AppError} from '../helpers/responses.js';
+
+export function verifyRoles(rolesPermitidos) {
+    return (req, res, next) => {
+        try {
+            const userRole = req.user?.rol || null;
+
+            if (!userRole) {
+                return next(new AppError("Token invalido o expirado", 401));
+            }
+
+            if (!rolesPermitidos.includes(userRole)) {
+                const validRolesNames = rolesPermitidos.join(", ");
+                return next(new AppError(`Acceso denegado: se necesitan privilegios de ${validRolesNames}`, 403));
+            }
+
+            next();
+        } catch (error) {
+            next(error);
+        }
+    };
+}
