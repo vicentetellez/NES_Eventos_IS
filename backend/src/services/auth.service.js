0f8b2a4f819291c00bec20ebf198da6983db76d0
Vicente Téllez <vicente.tellez2401@alumnos.ubiobio.cl>
2026-09-17 01:11:32 -0300

    feat(auth): agrega la funcion login que da el JWT Token

---
+++ backend/src/services/auth.service.js
@@ -0,0 +1,31 @@
+import argon2 from "argon2";
+import jwt from "jsonwebtoken";
+import prisma from '../config/prisma.js';
+import { JWT_SECRET } from "../config/configEnv.js";
+import { AppError } from "../helpers/responses.js";
+
+
+export async function loginService(data){
+    const user = await prisma.usuario.findUnique({ select: { rut: true, password: true, rol: true }, where: { rut: data.rut } });
+    if (!user) {
+        throw new AppError("RUT o contraseña incorrectos.", 401);
+    }
+        
+    const isPasswordValid = await argon2.verify(user.password, data.password);
+    if (!isPasswordValid) {
+        throw new AppError("RUT o contraseña incorrectos.", 401);
+    }
+
+    const token = jwt.sign(
+        { rut: user.rut },
+        JWT_SECRET,
+        { expiresIn: "1h" }
+    );
+
+    const userDTO = {
+        rut: user.rut,
+        rol: user.rol
+    };
+
+    return ({ user: userDTO, token });
+}
