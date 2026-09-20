4fa0a2f7e8cb54bc461d95bda1b6be5cd539415f
Vicente Téllez <vicente.tellez2401@alumnos.ubiobio.cl>
2026-09-20 18:58:32 -0300

    feat: agrega rutas para centralizar las rutas de tipo_evento, centro_evento y banqueteria

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes.js';
 import usuarioRoutes from './usuario.routes.js';
+import tipoEventoRoutes from './tipoEvento.routes.js';
+import centroEventoRoutes from './centroEvento.routes.js';
+import banqueteriaRoutes from './banqueteria.routes.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/usuario', usuarioRoutes);
+router.use('/tipo-evento', tipoEventoRoutes);
+router.use('/centro-evento', centroEventoRoutes);
+router.use('/banqueteria', banqueteriaRoutes);
 
 export default router;
